179a55f547546475c2015e8790f7e51b12621d4b
jaredoconnell <joconnel@redhat.com>
2023-04-28 15:57:22 -0400

    Simplify pipes

--- connector.go
+++ connector.go
@@ -33,52 +33,14 @@ func (p pluginConnection) Close() error {
 	panic("Not implemented. Careful to prevent goroutine leak")
 }
 
-type stdinSubstitute struct {
-	reader io.Reader
-}
-
-func (s stdinSubstitute) Read(p []byte) (n int, err error) {
-	return s.reader.Read(p)
-}
-
-func (s stdinSubstitute) Close() error {
-	// We don't depend on this being closed.
-	// If the output is done, it should be closed.
-	panic("Not implemented")
-}
-
-func newStdinSubstitute() (stdinSubstitute, io.Writer) {
-	stdinReader, stdinWriter := io.Pipe()
-	return stdinSubstitute{stdinReader}, stdinWriter
-}
-
-type stdoutSubstitute struct {
-	writer io.Writer
-}
-
-func (s stdoutSubstitute) Write(p []byte) (n int, err error) {
-	return s.writer.Write(p)
-}
-
-func (s stdoutSubstitute) Close() error {
-	// We don't depend on this being closed.
-	// If the output is done, it should be closed.
-	panic("Not implemented")
-}
-
-func newStdoutSubstitute() (stdoutSubstitute, io.Reader) {
-	stdoutReader, stdoutWriter := io.Pipe()
-	return stdoutSubstitute{stdoutWriter}, stdoutReader
-}
-
 func (c *connector) Deploy(ctx context.Context, image string) (deployer.Plugin, error) {
 	c.logger.Infof("Mimicking deployment of a plugin with image %s for testing", image)
 
 	// Simulate how it takes time to start the deployment.
 	time.Sleep(time.Duration(c.config.DeployTime) * time.Millisecond)
 
-	stdinSub, stdinWriter := newStdinSubstitute()
-	stdoutSub, stdoutReader := newStdoutSubstitute()
+	stdinSub, stdinWriter := io.Pipe()
+	stdoutReader, stdoutSub := io.Pipe()
 
 	// TODO: Allow plugin crash simulation by terminating the ATP server early.
 	go func() {
